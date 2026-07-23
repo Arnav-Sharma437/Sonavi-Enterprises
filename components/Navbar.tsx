49792b883e7207ea9c0397d33ea4f01f724a72a4
Arnav Sharma <aarnavsharma437@gmail.com>
2026-07-23 23:36:06 +0530

    Set navbar container height to 100px and update logo style to 200px x 100px

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -46,7 +46,7 @@ export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 w-full border-b border-gray-200/80 bg-white/80 backdrop-blur-md">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-auto py-4 items-center justify-between">
+        <div className="flex h-[100px] items-center justify-between">
           {/* Logo */}
           <div className="flex-shrink-0">
             <Link href="/" className="flex items-center">
@@ -56,7 +56,7 @@ export default function Navbar() {
                 width={200}
                 height={200}
                 priority
-                className="w-[200px] h-[200px] object-contain"
+                className="w-[200px] h-[100px] object-contain"
               />
             </Link>
           </div>
